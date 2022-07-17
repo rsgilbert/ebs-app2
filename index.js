46433455f0478c26e1c867785b75f3612487a41a
Gilbert <gssenyonjo@gmail.com>
2022-07-17 15:20:06 +0300

    correction

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(morgan('tiny'))
 app.get('/', (req, res) => res.send('Success, running'));
 app.get('/test', (req, res) => res.send("Test successful. App is running"));
 
-const port = process.env.PORT || 3001cd;
+const port = process.env.PORT || 3001;
 app.listen(port, ()=> console.log('running on port', port));
 
 
