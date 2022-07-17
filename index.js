94aa8f105102d23d36a0997c001d36ec1ce2872c
Gilbert <gssenyonjo@gmail.com>
2022-07-17 15:31:37 +0300

    added staticfiles

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(morgan('tiny'))
 
 app.get('/', (req, res) => res.send('Success, running'));
 app.get('/test', (req, res) => res.send("Test successful. App is running"));
-
+app.get('/romero', (req, res) => res.send("I am romero lukaku"))
 const port = process.env.PORT || 3001;
 app.listen(port, ()=> console.log('running on port', port));
 
