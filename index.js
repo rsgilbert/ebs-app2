dde5f00625b36463ea8cb9eb0d1a18b1de703341
Gilbert <gssenyonjo@gmail.com>
2022-07-17 14:56:25 +0300

    setup basic express app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => res.send('Success, running'));
+app.get('/test', (req, res) => res.send("Test successful. App is running"));
+
+const port = process.env.PORT || 3001cd;
+app.listen(port, ()=> console.log('running on port', port));
+
+
+
+
